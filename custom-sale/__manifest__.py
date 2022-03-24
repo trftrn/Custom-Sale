b0056f6ff951a8e961c131638752581f79605bf4
unknown <vini@balicipta.com>
2022-03-24 19:58:50 +0700

    custom-sale

---
+++ custom-sale/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Custom Sale',
+    'version': '14.0.0.1',
+    'summary': 'coba custom sale',
+    'description': 'coba doang custom sale',
+    'category': 'technical',
+    'author': 'vini',
+    'depends': ['sale'],
+    'data': [
+        'security/group.xml',
+        'view/custom-saleorder-view.xml',
+    ],
+    # 'demo': [''],
+    'installable': True,
+    'auto_install': False,
+
+}
